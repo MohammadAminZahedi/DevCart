e5d3dd205b0d9dc933dd8afe2a290b9ece70ab88
MohammadAminZahedi <amin.zahedi.2002@gmail.com>
2021-08-23 14:37:25 +0430

    Projects & LatesArticles

---
+++ DevCart_Mvc/ViewCompnents/LastestArticlesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using DevCart_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCart_Mvc.ViewCompnents
+{
+    public class LastestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(1,"آموزش ASP.NET Core","کاملترین پکیج آموزشی asp.net core به زبان فارسی","blog-post-thumb-1.jpg"),
+                new Article(2,"آموزش Git & Github","کاملترین پکیج آموزشی Git & Github به زبان فارسی","blog-post-thumb-2.jpg"),
+                new Article(3,"آموزش Onion Architecture","کاملترین پکیج آموزشی Onion Architecture به زبان فارسی","blog-post-thumb-3.jpg"),
+                new Article(4,"آموزش طزاخی سایت","کاملترین پکیج آموزشی طزاخی سازت به زبان فارسی","blog-post-thumb-4.jpg"),
+
+            };
+            return View("LastestArticles",articles);
+        }
+    }
+}
---
+++ DevCart_Mvc/ViewCompnents/ProjectsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using DevCart_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCart_Mvc.ViewCompnents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                new Project(1,"تاکسی","درخواست تاکسی برای سفرهای درون شهری","project-1.jpg","Snapp"),
+                new Project(2,"زود قود","درخواست آنلاین غذا","project-2.jpg","ZoodFood"),
+                new Project(3,"مدارس","سیستم یکپارچه مدیریت مدارس","project-3.jpg","MONOP"),
+                new Project(4,"فضاپیما","برنامه مدیریت فضاپیما های ناسا","project-4.jpg","NASA"),
+            };
+            return View("Projects",projects);
+        }
+    }
+}
