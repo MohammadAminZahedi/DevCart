e54bf0023a84e1d17c2058f979d4bbe056400985
MohammadAminZahedi <amin.zahedi.2002@gmail.com>
2021-08-23 11:44:47 +0430

    project section from index page moved to partial view project

---
+++ DevCart_Mvc/Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace DevCart_Mvc.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DevCart_Mvc/Models/IndexModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DevCart_Mvc.Models
+{
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+
+    }
+}
